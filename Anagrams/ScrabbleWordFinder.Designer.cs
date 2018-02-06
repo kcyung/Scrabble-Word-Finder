4f99b62505f5a06deae64c014be4281e2d578a79
Ka Chun Yung <kcyung780@hotmail.com>
2018-02-06 11:08:01 -0700

    Add project files.

---
+++ Anagrams/ScrabbleWordFinder.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Anagrams
+{
+    partial class ScrabbleWordFinder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UI_TB_Input = new System.Windows.Forms.TextBox();
+            this.UI_LB_Words = new System.Windows.Forms.ListBox();
+            this.UI_LBL_EnterTiles = new System.Windows.Forms.Label();
+            this.UI_LBL_MaxCharacters = new System.Windows.Forms.Label();
+            this.UI_BTN_Points = new System.Windows.Forms.Button();
+            this.UI_BTN_Length = new System.Windows.Forms.Button();
+            this.UI_LBL_BlanksWorthNothing = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // UI_TB_Input
+            // 
+            this.UI_TB_Input.Location = new System.Drawing.Point(26, 36);
+            this.UI_TB_Input.MaxLength = 15;
+            this.UI_TB_Input.Name = "UI_TB_Input";
+            this.UI_TB_Input.Size = new System.Drawing.Size(256, 20);
+            this.UI_TB_Input.TabIndex = 0;
+            // 
+            // UI_LB_Words
+            // 
+            this.UI_LB_Words.FormattingEnabled = true;
+            this.UI_LB_Words.Location = new System.Drawing.Point(26, 109);
+            this.UI_LB_Words.Name = "UI_LB_Words";
+            this.UI_LB_Words.ScrollAlwaysVisible = true;
+            this.UI_LB_Words.Size = new System.Drawing.Size(256, 290);
+            this.UI_LB_Words.TabIndex = 1;
+            // 
+            // UI_LBL_EnterTiles
+            // 
+            this.UI_LBL_EnterTiles.AutoSize = true;
+            this.UI_LBL_EnterTiles.Location = new System.Drawing.Point(23, 17);
+            this.UI_LBL_EnterTiles.Name = "UI_LBL_EnterTiles";
+            this.UI_LBL_EnterTiles.Size = new System.Drawing.Size(148, 13);
+            this.UI_LBL_EnterTiles.TabIndex = 2;
+            this.UI_LBL_EnterTiles.Text = "Enter Tiles (use \'?\' for blanks):";
+            // 
+            // UI_LBL_MaxCharacters
+            // 
+            this.UI_LBL_MaxCharacters.AutoSize = true;
+            this.UI_LBL_MaxCharacters.Location = new System.Drawing.Point(182, 59);
+            this.UI_LBL_MaxCharacters.Name = "UI_LBL_MaxCharacters";
+            this.UI_LBL_MaxCharacters.Size = new System.Drawing.Size(97, 13);
+            this.UI_LBL_MaxCharacters.TabIndex = 3;
+            this.UI_LBL_MaxCharacters.Text = "max. 15 characters";
+            // 
+            // UI_BTN_Points
+            // 
+            this.UI_BTN_Points.Location = new System.Drawing.Point(26, 80);
+            this.UI_BTN_Points.Name = "UI_BTN_Points";
+            this.UI_BTN_Points.Size = new System.Drawing.Size(128, 22);
+            this.UI_BTN_Points.TabIndex = 4;
+            this.UI_BTN_Points.Text = "Sort By Points";
+            this.UI_BTN_Points.UseVisualStyleBackColor = true;
+            this.UI_BTN_Points.Click += new System.EventHandler(this.UI_BTN_Points_Click);
+            // 
+            // UI_BTN_Length
+            // 
+            this.UI_BTN_Length.Location = new System.Drawing.Point(160, 80);
+            this.UI_BTN_Length.Name = "UI_BTN_Length";
+            this.UI_BTN_Length.Size = new System.Drawing.Size(119, 22);
+            this.UI_BTN_Length.TabIndex = 5;
+            this.UI_BTN_Length.Text = "Sort By Word Length";
+            this.UI_BTN_Length.UseVisualStyleBackColor = true;
+            this.UI_BTN_Length.Click += new System.EventHandler(this.UI_BTN_Length_Click);
+            // 
+            // UI_LBL_BlanksWorthNothing
+            // 
+            this.UI_LBL_BlanksWorthNothing.AutoSize = true;
+            this.UI_LBL_BlanksWorthNothing.Location = new System.Drawing.Point(130, 402);
+            this.UI_LBL_BlanksWorthNothing.Name = "UI_LBL_BlanksWorthNothing";
+            this.UI_LBL_BlanksWorthNothing.Size = new System.Drawing.Size(152, 13);
+            this.UI_LBL_BlanksWorthNothing.TabIndex = 6;
+            this.UI_LBL_BlanksWorthNothing.Text = "(Blank Tiles are worth 0 points)";
+            // 
+            // ScrabbleWordFinder
+            // 
+            this.AcceptButton = this.UI_BTN_Points;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 421);
+            this.Controls.Add(this.UI_LBL_BlanksWorthNothing);
+            this.Controls.Add(this.UI_BTN_Length);
+            this.Controls.Add(this.UI_BTN_Points);
+            this.Controls.Add(this.UI_LBL_MaxCharacters);
+            this.Controls.Add(this.UI_LBL_EnterTiles);
+            this.Controls.Add(this.UI_LB_Words);
+            this.Controls.Add(this.UI_TB_Input);
+            this.Name = "ScrabbleWordFinder";
+            this.Text = "Scrabble Word Finder";
+            this.Load += new System.EventHandler(this.WordSearch_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UI_TB_Input;
+        private System.Windows.Forms.ListBox UI_LB_Words;
+        private System.Windows.Forms.Label UI_LBL_EnterTiles;
+        private System.Windows.Forms.Label UI_LBL_MaxCharacters;
+        private System.Windows.Forms.Button UI_BTN_Points;
+        private System.Windows.Forms.Button UI_BTN_Length;
+        private System.Windows.Forms.Label UI_LBL_BlanksWorthNothing;
+    }
+}
+
---
+++ Anagrams/ScrabbleWordFinder.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Anagrams
+{
+    public partial class ScrabbleWordFinder : Form
+    {
+        // Dictionary with the key to hold every word, and value to hold a dictionary with the char and char count
+        Dictionary<string, Dictionary<char, int>> masterDictionary = new Dictionary<string, Dictionary<char, int>>();
+
+        // Dictionary with the point value for every character/tile available
+        Dictionary<char, int> Point = new Dictionary<char, int>();
+
+        // Dictionary with the potential words and point value
+        Dictionary<string, int> Results = new Dictionary<string, int>();
+
+
+        public ScrabbleWordFinder()
+        {
+            InitializeComponent();
+        }
+
+        // Upon loading program, read from file and intialize dictionaries
+        private void WordSearch_Load(object sender, EventArgs e)
+        {
+            // Read all words from text file and load it into master dictionary
+            try
+            {
+                StreamReader sr = new StreamReader("words.txt");
+                while (!sr.EndOfStream)
+                {
+                    string word = sr.ReadLine().Trim().ToLower();
+
+                    // Skip possible duplicates due to ToLower function
+                    if (!masterDictionary.ContainsKey(word))
+                        masterDictionary.Add(word, CharCounter(word));
+                }
+            }
+            catch (Exception error)
+            { Console.WriteLine("Can not read file: " + error); }
+
+            // Scrabble point values (all set to 1 and then adjusted)
+            for (char c = 'a'; c < 'z' + 1; ++c)
+                Point.Add(c, 1);
+
+            Point['d'] = Point['g'] = 2;
+            Point['b'] = Point['c'] = Point['m'] = Point['p'] = 3;
+            Point['f'] = Point['h'] = Point['v'] = Point['w'] = Point['y'] = 4;
+            Point['k'] = 5;
+            Point['j'] = Point['x'] = 8;
+            Point['z'] = 10;
+
+            // Used '?' for blank spaces
+            Point.Add('?', 0);
+        }
+
+        // Creates a dictionary from a string input with char of every letter as key, and the count as value
+        private Dictionary<char, int> CharCounter (string s)
+        {
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+
+            foreach (char c in s)
+            {
+                if (dictionary.ContainsKey(c))
+                    ++dictionary[c];
+                else
+                    dictionary.Add(c, 1);
+            }
+
+            return dictionary;
+        }
+
+        // Event handler that sorts possible words by highest point value
+        private void UI_BTN_Points_Click(object sender, EventArgs e)
+        {
+            // Get user input and update possible results
+            UpdateResultList();
+            
+            // Order results by point value and publish to list box
+            Results = Results.OrderByDescending(o => o.Value).ToDictionary(o => o.Key, o => o.Value);
+            foreach (KeyValuePair<string, int> kvp in Results)
+                UI_LB_Words.Items.Add(string.Format("{0:d3}: {1}", kvp.Value, kvp.Key));
+        }
+
+        // Event handler to sort possible words in order from longest word length
+        private void UI_BTN_Length_Click(object sender, EventArgs e)
+        {
+            // Get user input and update possible result
+            UpdateResultList();
+
+            // Order results by word length and publish to list box
+            Results = Results.OrderByDescending(o => o.Key.Length).ThenByDescending(o => o.Value).
+                ToDictionary(o => o.Key, o => o.Value);
+            foreach (KeyValuePair<string, int> kvp in Results)
+                UI_LB_Words.Items.Add(string.Format("{0:d3}: {1}", kvp.Value, kvp.Key));
+        }
+
+        // Checks user input, and updates master list of possilbe results
+        private void UpdateResultList()
+        {
+            // Clear all previous results
+            UI_LB_Words.Items.Clear();
+
+            // Receive user input as a string in lower case ordered by char
+            string userString = string.Concat(UI_TB_Input.Text.Trim().ToLower().OrderBy(o => o));
+
+            if (userString.Length < 2)
+            {
+                UI_LB_Words.Items.Add("You must enter at least two tiles");
+                return;
+            }
+
+            // Check to ensure all characters are a letter or a '?' for a blank square
+            foreach (char c in userString)
+                if (!char.IsLetter(c) && (c != '?'))
+                {
+                    UI_LB_Words.Items.Add("You may only use letters and/or a '?' for a blank");
+                    return;
+                }
+
+            // Make a list of words to reduce searches
+            List<string> copy = new List<string>(masterDictionary.Keys);
+            copy.RemoveAll(o => o.Length < 2 || o.Length > userString.Length);
+
+            // Dictionary to hold possible words and point values
+            Dictionary<string, int> matches = new Dictionary<string, int>();
+
+            // Dictionary of character and count of user input string
+            Dictionary<char, int> userDict = CharCounter(userString);
+
+            // Check if there are more than two blank spaces entered - invalid
+            if (userString.Length > 2 && userDict.ContainsKey('?') && userDict['?'] > 2)
+            {
+                UI_LB_Words.Items.Clear();
+                UI_LB_Words.Items.Add("You may only have a maximum of two blank spaces [?]");
+                return;
+            }
+
+            // ***** Valid User Input - Generate possible results with their point values ***** //
+            // Iterate through all words at least two characters to length of input
+            foreach (string word in copy)
+            {
+                bool possibleMatch = true;
+
+                // Dictionary of current word in with kvp <char, char - count> 
+                Dictionary<char, int> wordDict = masterDictionary[word];
+
+                int Blanks = userDict.ContainsKey('?') ? userDict['?'] : 0;
+                int PointsForBlanks = 0;
+
+                // Iterate through each kvp pair for the current word
+                foreach (KeyValuePair<char, int> kvp in wordDict)
+                {
+                    if (!userDict.ContainsKey(kvp.Key))
+                    {
+                        Blanks -= wordDict[kvp.Key];
+                        PointsForBlanks += Point[kvp.Key] * wordDict[kvp.Key];
+                    }
+                    // char exists in the user input; but word requires more characters
+                    else if (userDict[kvp.Key] < wordDict[kvp.Key])
+                    {
+                        Blanks -= wordDict[kvp.Key] - userDict[kvp.Key];
+                        PointsForBlanks += Point[kvp.Key] * (wordDict[kvp.Key] - userDict[kvp.Key]);
+                    }
+                    // Can not possibly make this word from user tiles, skip this word
+                    if (Blanks < 0)
+                    {
+                        possibleMatch = false;
+                        break;
+                    }
+                }
+
+                if (possibleMatch)
+                    matches.Add(word, word.Sum(o => Point[o]) - PointsForBlanks);
+            }
+
+            Results = new Dictionary<string,int>(matches);
+        }
+    }
+}
